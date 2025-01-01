01cdff11a3ac56523941188c2ad4f1ab308ca3d6
Kevin Becker <kevinfjbecker@gmail.com>
2025-01-01 11:26:09 +0100

    Check by index of feedback

--- main.mjs
+++ main.mjs
@@ -1,31 +1,59 @@
 import * as fs from 'fs'
 
-const wordsFileContent = fs.readFileSync('sgb-words.txt').toString()
+///////////////////////////////////////////////////////////////////////////////
 
+const wordsFileContent = fs.readFileSync('sgb-words.txt').toString()
 const words = wordsFileContent.split('\n')
 
-const lettersContained = ''
-const lettersExcluded = ''
+///////////////////////////////////////////////////////////////////////////////
+
+const information = JSON.parse(fs.readFileSync('info.json'))
+
+///////////////////////////////////////////////////////////////////////////////
 
 const contains = (c) => (s) => s.includes(c)
 const excluded = (c) => (s) => ! s.includes(c)
+const characterAt = (c, i) => (s) => s[i] === c
+const noCharacterAt = (c, i) => (s) => s[i] !== c
+
+///////////////////////////////////////////////////////////////////////////////
 
 let filteredWords = words.slice()
-for(const c of lettersContained)
+for(const { guess, response } of information)
 {
-    filteredWords = filteredWords.filter(contains(c))
+    for(let i = 0; i < guess.length; i++)
+    {
+        if(response[i] === '0')
+        {
+            filteredWords = filteredWords.filter(excluded(guess[i]))
+        }
+        if(response[i] === '1')
+        {
+            filteredWords = filteredWords.filter(noCharacterAt(guess[i], i))
+            filteredWords = filteredWords.filter(contains(guess[i]))
+            
+        }
+        if(response[i] === '2')
+            {
+                filteredWords = filteredWords.filter(characterAt(guess[i], i))
+                filteredWords = filteredWords.filter(contains(guess[i]))
+        }
+    }
 }
-for(const c of lettersExcluded)
-{
-    filteredWords = filteredWords.filter(excluded(c))
-}
-console.log(`There are ${filteredWords.length} possible words`)
-console.log(`The possible words are ${filteredWords}`)
 
-// let entropyWords = words.slice()
-// for(const c of lettersContained.concat(lettersExcluded))
-// {
-//     entropyWords = entropyWords.filter(excluded(c))
-// }
-// console.log(`There are ${entropyWords.length} entropy words`)
-// console.log(`The entropy words are ${entropyWords}`)
+///////////////////////////////////////////////////////////////////////////////
+
+if(filteredWords.length > 1)
+{
+    console.log(`There are ${filteredWords.length} possible words`)
+    console.log(`The possible words are ${filteredWords}`)
+}
+if(filteredWords.length === 1)
+{
+    console.log(`There is ${filteredWords.length} possible word`)
+    console.log(`The word is ${filteredWords}`)
+}
+if(filteredWords.length === 0)
+{
+    console.log('There are no possible words')
+}
