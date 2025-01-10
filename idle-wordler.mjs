95f5ca587f505ca9b222346f50beaa3e6061e59d
Kevin Becker <kevinfjbecker@gmail.com>
2025-01-10 15:25:48 +0100

    Update idle-wordler.mjs

--- idle-wordler.mjs
+++ idle-wordler.mjs
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import { argv } from 'process'
 
 import { getHighLetterFrequencyWords } from './letter-counts.mjs'
 import { getMask } from './guess-response.mjs'
@@ -11,7 +12,23 @@ const words = wordsFileContent.split('\n').filter(w => !! w)
 
 ///////////////////////////////////////////////////////////////////////////////
 
-const answer = words[Math.floor(Math.random() * words.length)]
+let answer
+
+if(argv.length > 2 && argv[2] === '-a' && argv[3]?.length === 5)
+{
+    answer = argv[3]
+
+    // debug
+    // argv.forEach((val, index) => {
+    //     console.log(`${index}: ${val}`);
+    // });
+}
+else
+{
+    answer = words[Math.floor(Math.random() * words.length)]
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 console.log(answer)
 
@@ -22,23 +39,23 @@ let filteredWords = words
 for(let i = 1; i <= 6; i++)
 {
     filteredWords = getFilteredWords(filteredWords, information)
-    
+
     const guess = getHighLetterFrequencyWords(filteredWords)
         .toSorted((a,b) => b.cumulativeLetterCount - a.cumulativeLetterCount)
         [0].word
-    
+
     const response = getMask(answer, guess)
-    
+
     information.push({guess, response})
 
     console.log(`(${i}) ${guess} - ${response} [${filteredWords.length} possible guesses]`)
-    
+
     if(response === '22222')
     {
         console.log('\nWinner-winner, chicken dinner!')
         break
     }
-    
+
 }
 
 if(information[information.length - 1].response !== '22222')
